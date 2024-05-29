9f02641809eea8e666e7f46fd9069ca7889cf74d
Jonathan <jdomingo-2022206@kinal.edu.gt>
2024-05-29 14:14:20 -0600

    Just a little fix

--- src/middlewares/db-validators.js
+++
@@ -1,5 +0,0 @@
-import User from '../modules/user/user.model.js'
-import Community from '../modules/community/community.model.js'
-import Post from '../modules/post/post.model.js'
-import Comment from '../modules/comment/comment.model.js'
-
