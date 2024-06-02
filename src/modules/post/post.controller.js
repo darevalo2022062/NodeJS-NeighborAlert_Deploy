ee187bd1ab08b58b6a53e8c7f44b1b6da1d53d53
mvalladares-2019607 <mvalladares-2019607@kinal.edu.gt>
2024-06-02 13:10:21 -0600

    export collections and endpoint

--- src/modules/post/post.controller.js
+++ src/modules/post/post.controller.js
@@ -8,7 +8,7 @@ const handleResponse = (res, promise) => {
         .then(data => res.status(200).json(data))
         .catch(error => {
             console.error('Error:', error);
-            res.status(500), json({ error: 'Internal server error' });
+            res.status(500), json({error: 'Internal server error'});
         });
 };
 
