173067764b416f30697ae8d02e9d5ebcd0c554ba
esantos2019254 <esantos-2019254@kinal.edu.gt>
2024-06-03 12:16:04 -0600

    Validations were created for users

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -1,5 +1,7 @@
 import bcryptjs from 'bcryptjs';
 import User from './user.model.js';
+import { validateAdmin } from '../../helpers/data-methods.js';
+import { isToken } from '../../helpers/tk-methods.js';
 
 const handleResponse = (res, promise) => {
     promise
@@ -10,30 +12,64 @@ const handleResponse = (res, promise) => {
         });
 };
 
+const validateUserRequest = async (req, res) => {
+    try {
+        const user = await isToken(req, res);
+        if (user.role === 'ADMIN') {
+            return true;
+        }
+        return false;  
+    } catch (error) {
+        return res.status(400).json({ error: error.message });
+    }
+};
 
 export const getUsers = async (req, res) => {
-    handleResponse(res, User.find({ status: true }));
+    const isAdmin = await validateUserRequest(req, res);
+    if (isAdmin) {
+        handleResponse(res, User.find({ status: true }));
+    } else {
+        return res.status(403).json({ error: "Access denied, you are not an ADMIN" });
+    }
 };
 
 export const getUser = async (req, res) => {
     const { id } = req.params;
-    handleResponse(res, User.findById(id));
+    const user = await isToken(req, res);
+    
+    if (user.role === 'ADMIN' || user._id.toString() === id) {
+        handleResponse(res, User.findById(id));
+    } else {
+        return res.status(403).json({ error: "You can only view your account" });
+    }
 };
 
 export const updateUser = async (req, res) => {
     const { id } = req.params;
-    const { name, lastName, phone, pass, img, idCommunity } = req.body;
-    const newData = { name, lastName, phone, img, idCommunity };
+    const user = await isToken(req, res);
+    
+    if (user._id.toString() === id) {
+        const { name, lastName, phone, pass, img, idCommunity } = req.body;
+        const newData = { name, lastName, phone, img, idCommunity };
 
-    if (pass) {
-        const salt = bcryptjs.genSaltSync();
-        newData.pass = bcryptjs.hashSync(pass, salt);
+        if (pass) {
+            const salt = bcryptjs.genSaltSync();
+            newData.pass = bcryptjs.hashSync(pass, salt);
+        }
+
+        handleResponse(res, User.findOneAndUpdate({ _id: id, status: true }, { $set: newData }, { new: true }));
+    } else {
+        return res.status(403).json({ error: "You can only edit your account" });
     }
-
-    handleResponse(res, User.findOneAndUpdate({ _id: id, status: true }, { $set: newData }, { new: true }));
 };
 
 export const deleteUser = async (req, res) => {
     const { id } = req.params;
-    handleResponse(res, User.findByIdAndUpdate(id, { status: false }, { new: true }));
+    const user = await isToken(req, res);
+    
+    if (user._id.toString() === id || user.role === 'ADMIN') {
+        handleResponse(res, User.findByIdAndUpdate(id, { status: false }, { new: true }));
+    } else {
+        return res.status(403).json({ error: "You can only delete your account" });
+    }
 };
