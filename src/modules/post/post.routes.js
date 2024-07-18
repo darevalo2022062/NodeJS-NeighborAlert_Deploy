08213384b41af3ffeb92c0c6380ab9de4c66153b
arodriguez-20199620 <arodriguez-2019620@kinal.edu.gt>
2024-07-18 14:57:41 -0600

    add publications with images

--- src/modules/post/post.routes.js
+++ src/modules/post/post.routes.js
@@ -1,41 +1,41 @@
-import { Router } from "express"; 
-import { check } from "express-validator"; 
-import { createPost, getMyPost, deletePost, updatePost, getPostByCommunity } from "./post.controller.js"; 
+import { Router } from "express";
+import { check } from "express-validator";
+import { createPost, getMyPost, deletePost, updatePost, getPostByCommunity } from "./post.controller.js";
 import { validateJWT } from "../../middlewares/validate-jwt.js";
 import { validateFields } from "../../middlewares/validate-fields.js";
+import upload from '../../middlewares/uploadMiddlewares.js';
 
 
-const router = Router(); 
+const router = Router();
 
 router.post(
     "/",
     [
         validateJWT,
-        check("idCommunity", "idCommunity is required").isMongoId(), 
-        check("title", "title is required").not().isEmpty(), 
+        upload.array('file', 5),
+        check("idCommunity", "idCommunity is required").isMongoId(),
         check("content", "content is required").not().isEmpty(),
         check("anonymous", "anonymous is required").not().isEmpty(),
-        check("category", "category is required").not().isEmpty(), 
-        
+        check("category", "category is required").not().isEmpty(),
         validateFields
     ],
     createPost
 );
 
 router.put(
-    "/:id", 
+    "/:id",
     [
         validateJWT,
-        check("title", "title is required").not().isEmpty(), 
+        check("title", "title is required").not().isEmpty(),
         check("content", "content is required").not().isEmpty(),
-        check("category", "category is required").not().isEmpty(), 
+        check("category", "category is required").not().isEmpty(),
         validateFields
-    ], 
+    ],
     updatePost
 
 )
 
-router.get("/community", validateJWT, getPostByCommunity )
+router.get("/community", validateJWT, getPostByCommunity)
 
 router.get("/", validateJWT, getMyPost);
 
